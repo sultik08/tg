f4f62f941e3886b5573ed42a8342b5087b9c2e0c
sultik08 <148442703+sultik08@users.noreply.github.com>
2025-04-12 17:19:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+from telethon import TelegramClient, events
+import os
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+session_string = os.environ.get("SESSION_STRING")
+owner_username = os.environ.get("OWNER_USERNAME")
+
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
+
+KEYWORDS = ["ассистент", "съёмка", "работа в кино", "продюсер", "оплата", "дедлайн"]
+
+@client.on(events.NewMessage(incoming=True))
+async def handler(event):
+    if event.is_group or event.is_channel:
+        msg = event.message.message.lower()
+        if any(keyword in msg for keyword in KEYWORDS):
+            await client.send_message(owner_username, f"👀 Вакансия:
+
+{event.message.message}")
+
+print("🚀 Бот запущен...")
+client.start()
+client.run_until_disconnected()
